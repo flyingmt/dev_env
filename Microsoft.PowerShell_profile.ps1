35586c2ea4fe74696ff63d0e261fc1845ca892b4
flyingmt <flyingmt@gmail.com>
2021-12-14 18:58:39 +0900

    Completed Powershell Part

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,35 @@
+# Prompt
+Import-Module posh-git
+Import-Module oh-my-posh
+Set-PoshPrompt Paradox
+
+# Load prompt config
+$omp_config = Join-Path $PSScriptRoot ".\my.omp.json"
+oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
+
+# Terminal-Icons
+Import-Module -Name Terminal-Icons
+
+# PSReadLine
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+Set-PSReadLineOption -PredictionSource History
+
+# Fzf
+Import-Module PSFzf
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+# Alias
+Set-Alias vim nvim
+Set-Alias ll ls
+Set-Alias g git
+Set-Alias grep findstr
+Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
+Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
+
+# Utilities
+function which ($command) {
+  Get-Command -Name $command -ErrorAction SilentlyContinue |
+    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
